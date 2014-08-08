04714ab987f1055e78e0366c1b639db8adc6d685
Brad Hards <bradh@frogmouth.net>
2014-08-08 14:29:32 +1000

    Start adding a more complex NITF 2.0 test case

--- src/test/java/org/codice/nitf/filereader/Nitf20HeaderTest.java
+++ src/test/java/org/codice/nitf/filereader/Nitf20HeaderTest.java
@@ -106,4 +106,95 @@ public class Nitf20HeaderTest {
         is.close();
     }
 
+    @Test
+    public void testU1123() throws IOException, ParseException {
+        final String nitf20File = "/U_1123A.NTF";
+
+        assertNotNull("Test file missing", getClass().getResource(nitf20File));
+
+        InputStream is = getClass().getResourceAsStream(nitf20File);
+        NitfFile file = new NitfFile();
+        file.parse(is, EnumSet.allOf(ParseOption.class));
+        assertEquals(FileType.NITF_TWO_ZERO, file.getFileType());
+        assertEquals(1, file.getComplexityLevel());
+        assertEquals("", file.getStandardType());
+        assertEquals("U217G0J1", file.getOriginatingStationId());
+        assertEquals("1992-11-03 13:52:26", new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(file.getFileDateTime()));
+        assertEquals("This NITF message contains 5 images, 4 symbols, 4 labels and 1 text.", file.getFileTitle());
+        NitfFileSecurityMetadata securityMetadata = file.getFileSecurityMetadata();
+        assertNotNull(securityMetadata);
+        assertEquals(NitfSecurityClassification.UNCLASSIFIED, securityMetadata.getSecurityClassification());
+        assertNull(securityMetadata.getSecurityClassificationSystem());
+        assertEquals("", securityMetadata.getCodewords());
+        assertEquals("", securityMetadata.getControlAndHandling());
+        assertEquals("", securityMetadata.getReleaseInstructions());
+        assertNull(securityMetadata.getDeclassificationType());
+        assertNull(securityMetadata.getDeclassificationDate());
+        assertNull(securityMetadata.getDeclassificationExemption());
+        assertNull(securityMetadata.getDowngrade());
+        assertNull(securityMetadata.getDowngradeDate());
+        assertNull(securityMetadata.getClassificationText());
+        assertNull(securityMetadata.getClassificationAuthorityType());
+        assertEquals("", securityMetadata.getClassificationAuthority());
+        assertNull(securityMetadata.getClassificationReason());
+        assertEquals("", securityMetadata.getSecurityControlNumber());
+        assertEquals("999998", securityMetadata.getDowngradeDateOrSpecialCase());
+        assertEquals("This message will not need a downgrade.", securityMetadata.getDowngradeEvent());
+
+        assertEquals("00001", file.getFileSecurityMetadata().getFileCopyNumber());
+        assertEquals("00001", file.getFileSecurityMetadata().getFileNumberOfCopies());
+        assertEquals("JITC", file.getOriginatorsName());
+        assertEquals("(602) 538-5458", file.getOriginatorsPhoneNumber());
+        assertEquals(5, file.getNumberOfImageSegments());
+        assertEquals(0, file.getNumberOfGraphicSegments());
+        assertEquals(4, file.getNumberOfSymbolSegments());
+        assertEquals(4, file.getNumberOfLabelSegments());
+        assertEquals(1, file.getNumberOfTextSegments());
+        assertEquals(0, file.getNumberOfDataExtensionSegments());
+
+        NitfImageSegment imageSegment1 = file.getImageSegment(1);
+        assertNotNull(imageSegment1);
+
+        NitfImageSegment imageSegment2 = file.getImageSegment(2);
+        assertNotNull(imageSegment2);
+
+        NitfImageSegment imageSegment3 = file.getImageSegment(3);
+        assertNotNull(imageSegment3);
+
+        NitfImageSegment imageSegment4 = file.getImageSegment(4);
+        assertNotNull(imageSegment4);
+
+        NitfImageSegment imageSegment5 = file.getImageSegment(5);
+        assertNotNull(imageSegment5);
+
+        NitfTextSegment textSegment = file.getTextSegment(1);
+        assertNotNull(textSegment);
+        assertEquals("0000000001", textSegment.getTextIdentifier());
+        assertEquals(0, textSegment.getTextAttachmentLevel());
+        assertEquals("1990-06-07 21:11:36", new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(textSegment.getTextDateTime()));
+        assertEquals("This is the title of unclassified text file #1 in NITF message JR1_B.", textSegment.getTextTitle());
+        NitfSecurityMetadata textSecurityMetadata = textSegment.getSecurityMetadata();
+        assertNotNull(textSecurityMetadata);
+        assertEquals(NitfSecurityClassification.UNCLASSIFIED, textSecurityMetadata.getSecurityClassification());
+        assertNull(textSecurityMetadata.getSecurityClassificationSystem());
+        assertEquals("", textSecurityMetadata.getCodewords());
+        assertEquals("", textSecurityMetadata.getControlAndHandling());
+        assertEquals("", textSecurityMetadata.getReleaseInstructions());
+        assertNull(textSecurityMetadata.getDeclassificationType());
+        assertNull(textSecurityMetadata.getDeclassificationDate());
+        assertNull(textSecurityMetadata.getDeclassificationExemption());
+        assertNull(textSecurityMetadata.getDowngrade());
+        assertNull(textSecurityMetadata.getDowngradeDate());
+        assertNull(textSecurityMetadata.getClassificationText());
+        assertNull(textSecurityMetadata.getClassificationAuthorityType());
+        assertEquals("", textSecurityMetadata.getClassificationAuthority());
+        assertNull(textSecurityMetadata.getClassificationReason());
+        assertEquals("", textSecurityMetadata.getSecurityControlNumber());
+        assertEquals("999998", textSecurityMetadata.getDowngradeDateOrSpecialCase());
+        assertEquals("This text will never need downgrading.", textSecurityMetadata.getDowngradeEvent());
+        assertEquals(TextFormat.BASICCHARACTERSET, textSegment.getTextFormat());
+        assertEquals("123456\r\n", textSegment.getTextData());
+
+        is.close();
+    }
 }
