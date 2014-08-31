e54f35f8fd596fecf08c939616cc626ce1c8624d
Brad Hards <bradh@frogmouth.net>
2014-08-31 10:55:07 +1000

    Add tests for TRE entry operations.

---
+++ src/test/java/org/codice/imaging/nitf/core/TreTest.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright (c) Codice Foundation
+ * 
+ * This is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser
+ * General Public License as published by the Free Software Foundation, either version 3 of the
+ * License, or any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details. A copy of the GNU Lesser General Public License
+ * is distributed along with this program and can be found at
+ * <http://www.gnu.org/licenses/lgpl.html>.
+ * 
+ */
+package org.codice.imaging.nitf.core;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class TreTest {
+
+    @Test
+    public void testTreEntryAccessors1() {
+        TreEntry entry = new TreEntry("oldName", null);
+        assertNotNull(entry);
+
+        entry.setName("DANAME");
+        assertEquals("DANAME", entry.getName());
+
+        entry.setFieldValue("Some value");
+        assertEquals("Some value", entry.getFieldValue());
+    }
+
+    @Test
+    public void testTreEntryAccessors2() {
+        TreEntry entry = new TreEntry("ANAME", null);
+        assertNotNull(entry);
+
+        entry.initGroups();
+        assertEquals(0, entry.getGroups().size());
+        entry.addGroup(new TreGroup());
+        assertEquals(1, entry.getGroups().size());
+        // check it doesn't kill what we already have
+        entry.initGroups();
+        assertEquals(1, entry.getGroups().size());
+    }
+
+}
