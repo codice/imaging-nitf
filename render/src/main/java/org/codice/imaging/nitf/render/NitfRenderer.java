2767884d9bdbc608f8f217d12b14cf8a4ff48321
Don Cruver <donald.cruver@connexta.com>
2016-01-12 13:51:17 -0700

    img-66 Add support for mono JPEG image with mask blocks.
    This change uses the ImageMask to skip the appropriate blocks of a
    JPEG file.

--- render/src/main/java/org/codice/imaging/nitf/render/NitfRenderer.java
+++ render/src/main/java/org/codice/imaging/nitf/render/NitfRenderer.java
@@ -50,6 +50,10 @@ public class NitfRenderer {
                     imageData,
                     targetGraphic);
             break;
+        case JPEGMASK:
+            ImageMask imageMask = new ImageMask(imageSegmentHeader, imageData);
+            renderJPEG(imageSegmentHeader, imageData, targetGraphic, imageMask);
+            break;
         default:
             System.out.println("Unhandled image compression format: "
                     + imageSegmentHeader.getImageCompression());
