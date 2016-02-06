76aabba5e528d77a77ff4fa882ed50ca9a473bdd
Brad Hards <bradh@frogmouth.net>
2016-02-06 17:02:26 +1100

    [IMG-96] Add IREPBAND based mapping for RGB files.

--- render/src/main/java/org/codice/imaging/nitf/render/imagerep/Rgb24ImageRepresentationHandler.java
+++ render/src/main/java/org/codice/imaging/nitf/render/imagerep/Rgb24ImageRepresentationHandler.java
@@ -3,14 +3,15 @@ package org.codice.imaging.nitf.render.imagerep;
 import java.awt.image.BufferedImage;
 import java.awt.image.DataBuffer;
 import java.io.IOException;
-
-import javax.imageio.stream.ImageInputStream;
+import org.codice.imaging.nitf.core.image.ImageSegment;
 
 public class Rgb24ImageRepresentationHandler implements ImageRepresentationHandler {
+
     @Override
-    public void renderPixelBand(DataBuffer data, int pixelIndex, ImageInputStream imageInputStream, int bandIndex)
+    public void renderPixelBand(DataBuffer data, int pixelIndex, ImageSegment imageSegment, int bandIndex)
             throws IOException {
-        data.setElem(pixelIndex, data.getElem(pixelIndex) | (imageInputStream.read() << (8 * (2 - bandIndex))));
+        int pixelShift = getPixelShiftForBand(imageSegment, bandIndex);
+        data.setElem(pixelIndex, data.getElem(pixelIndex) | (imageSegment.getData().read() << pixelShift));
     }
 
     @Override
@@ -18,11 +19,31 @@ public class Rgb24ImageRepresentationHandler implements ImageRepresentationHandl
         return new BufferedImage(blockWidth, blockHeight, BufferedImage.TYPE_INT_ARGB);
     }
 
+    @Override
     public void applyPixelMask(DataBuffer data, int pixelIndex) {
         data.setElem(pixelIndex, data.getElem(pixelIndex) | 0xFF000000);
     }
 
+    @Override
     public void renderPadPixel(DataBuffer data, int pixelIndex) {
         data.setElem(pixelIndex, 0x00000000);
+    }
+
+    private int getPixelShiftForBand(ImageSegment imageSegment, int bandIndex) {
+        int leftShift;
+        switch (imageSegment.getImageBandZeroBase(bandIndex).getImageRepresentation()) {
+            case "R":
+                leftShift = 16;
+                break;
+            case "G":
+                leftShift = 8;
+                break;
+            case "B":
+                leftShift = 0;
+                break;
+            default:
+                leftShift = NOT_VISIBLE_MAPPED;
+        }
+        return leftShift;
     }
 }
