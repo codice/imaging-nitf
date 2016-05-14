faa2e3b91a176e7db2b3681fc8cf0c2b61e5737b
Brad Hards <bradh@frogmouth.net>
2016-05-14 14:03:19 +1000

    [IMG-136/157] Add SECTGA and EXOPTA TRE parsing support.

---
+++ core/src/test/java/org/codice/imaging/nitf/core/tre/EXOPTA_Test.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright (c) Codice Foundation
+ *
+ * This is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser
+ * General Public License as published by the Free Software Foundation, either version 3 of the
+ * License, or any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details. A copy of the GNU Lesser General Public License
+ * is distributed along with this program and can be found at
+ * <http://www.gnu.org/licenses/lgpl.html>.
+ *
+ */
+package org.codice.imaging.nitf.core.tre;
+
+import java.io.BufferedInputStream;
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import org.codice.imaging.nitf.core.common.NitfInputStreamReader;
+import org.codice.imaging.nitf.core.common.NitfReader;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ * Tests for EXOPTA TRE parsing.
+ *
+ * This TRE is described in STDI-0002 Annex E, Section 3.5.
+ *
+ */
+public class EXOPTA_Test {
+    
+    public EXOPTA_Test() {
+    }
+
+    @Test
+    public void SimpleEXOPTA() throws Exception {
+        InputStream inputStream = new ByteArrayInputStream("EXOPTA00107134203.3100512       82.31-26.971234567890ABCDEFGHIJKLMNOPQ     023  0000001003023487            +67.3349.2".getBytes());
+        BufferedInputStream bufferedStream = new BufferedInputStream(inputStream);
+        NitfReader nitfReader = new NitfInputStreamReader(bufferedStream);
+        TreCollectionParser parser = new TreCollectionParser();
+        TreCollection parseResult = parser.parse(nitfReader, 118, TreSource.ImageExtendedSubheaderData);
+        assertEquals(1, parseResult.getTREs().size());
+        Tre exopta = parseResult.getTREsWithName("EXOPTA").get(0);
+        assertNotNull(exopta);
+        assertNull(exopta.getRawData());
+        assertEquals(12, exopta.getEntries().size());
+        assertEquals("134", exopta.getFieldValue("ANGLE_TO_NORTH"));
+        assertEquals("203.3", exopta.getFieldValue("MEAN_GSD"));
+        assertEquals("00512", exopta.getFieldValue("DYNAMIC_RANGE"));
+        assertEquals("82.31", exopta.getFieldValue("OBL_ANG"));
+        assertEquals("-26.97", exopta.getFieldValue("ROLL_ANG"));
+        assertEquals("1234567890AB", exopta.getFieldValue("PRIME_ID"));
+        assertEquals("CDEFGHIJKLMNOPQ", exopta.getFieldValue("PRIME_BE"));
+        assertEquals(23, exopta.getIntValue("N_SEC"));
+        assertEquals(3, exopta.getIntValue("N_SEG"));
+        assertEquals("023487", exopta.getFieldValue("MAX_LP_SEG"));
+        assertEquals("+67.3", exopta.getFieldValue("SUN_EL"));
+        assertEquals("349.2", exopta.getFieldValue("SUN_AZ"));
+    }
+}
---
+++ core/src/test/java/org/codice/imaging/nitf/core/tre/SECTGA_Test.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright (c) Codice Foundation
+ *
+ * This is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser
+ * General Public License as published by the Free Software Foundation, either version 3 of the
+ * License, or any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details. A copy of the GNU Lesser General Public License
+ * is distributed along with this program and can be found at
+ * <http://www.gnu.org/licenses/lgpl.html>.
+ *
+ */
+package org.codice.imaging.nitf.core.tre;
+
+import java.io.BufferedInputStream;
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import org.codice.imaging.nitf.core.common.NitfInputStreamReader;
+import org.codice.imaging.nitf.core.common.NitfReader;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ * Tests for SECTGA TRE parsing.
+ *
+ * This TRE is described in STDI-0002 Annex E, Section 3.13.
+ *
+ */
+public class SECTGA_Test {
+    
+    public SECTGA_Test() {
+    }
+
+    @Test
+    public void SimpleSECTGA() throws Exception {
+        InputStream inputStream = new ByteArrayInputStream("SECTGA000281234567890ABCDEFGHIJKLMNOPQ0".getBytes());
+        BufferedInputStream bufferedStream = new BufferedInputStream(inputStream);
+        NitfReader nitfReader = new NitfInputStreamReader(bufferedStream);
+        TreCollectionParser parser = new TreCollectionParser();
+        TreCollection parseResult = parser.parse(nitfReader, 39, TreSource.ImageExtendedSubheaderData);
+        assertEquals(1, parseResult.getTREs().size());
+        Tre sectga = parseResult.getTREsWithName("SECTGA").get(0);
+        assertNotNull(sectga);
+        assertNull(sectga.getRawData());
+        assertEquals(2, sectga.getEntries().size());
+        assertEquals("1234567890AB", sectga.getFieldValue("SEC_ID"));
+        assertEquals("CDEFGHIJKLMNOPQ", sectga.getFieldValue("SEC_BE"));
+    }
+}
