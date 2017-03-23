f78779289e32b79e7d5130da7fec382a05ba59ef
Harrison Tarr <harrison.tarr@connexta.com>
2017-03-23 07:34:21 -0700

    IMG-225 Revert changes to RPFUtilities and formatting changes

--- core/src/main/java/org/codice/imaging/nitf/core/image/impl/RasterProductFormatUtilities.java
+++ core/src/main/java/org/codice/imaging/nitf/core/image/impl/RasterProductFormatUtilities.java
@@ -16,43 +16,36 @@ package org.codice.imaging.nitf.core.image.impl;
 
 import java.io.IOException;
 import java.io.InputStream;
-
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Unmarshaller;
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
-
 import org.codice.imaging.nitf.core.common.NitfFormatException;
 import org.codice.imaging.nitf.core.schema.Rpfs;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.w3c.dom.Document;
-import org.xml.sax.SAXException;
+
+
 
 /**
- * Utility class for Raster Product Format (RPF) handling.
- */
+    Utility class for Raster Product Format (RPF) handling.
+*/
 public class RasterProductFormatUtilities {
 
     private static final Logger LOG = LoggerFactory.getLogger(RasterProductFormatUtilities.class);
-
     private static final String RPF_LOAD_ERROR_MESSAGE = "Exception while loading RPF codes XML";
 
     private Rpfs rpfs = null;
 
     static final int CODE_LENGTH = 2;
-
     static final int EXPECTED_FILE_SUFFIX_LENGTH = 3;
 
     /**
-     * Construct utilities class.
-     * <p>
-     * This performs internal initialisation.
-     *
-     * @throws NitfFormatException if the internal initialisation fails.
-     */
+        Construct utilities class.
+        <p>
+        This performs internal initialisation.
+
+        @throws NitfFormatException if the internal initialisation fails.
+    */
     public RasterProductFormatUtilities() throws NitfFormatException {
         try (InputStream is = getClass().getResourceAsStream("/rpf_codes.xml")) {
             unmarshal(is);
@@ -66,42 +59,25 @@ public class RasterProductFormatUtilities {
     }
 
     /**
-     * Unmarshal input stream to RPFs list.
-     *
-     * @param inputStream the input stream to parse.
-     * @throws JAXBException if the unmarshalling of the XML fails.
-     */
+        Unmarshal input stream to RPFs list.
+
+        @param inputStream the input stream to parse.
+        @throws JAXBException if the unmarshalling of the XML fails.
+    */
     private void unmarshal(final InputStream inputStream) throws JAXBException {
-        DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
-
-        dbf.setExpandEntityReferences(false);
-
-        Document document;
-        try {
-            DocumentBuilder db = dbf.newDocumentBuilder();
-            document = db.parse(inputStream);
-        } catch (SAXException | IOException e) {
-            LOG.warn("Error parsing input. Set log to DEBUG for more information.");
-            LOG.debug("Error parsing input. {}", e);
-            throw new JAXBException(e);
-        } catch (ParserConfigurationException e) {
-            LOG.error("Error creating DocumentBuilder. Set log to DEBUG for more information.");
-            LOG.debug("Error creating DocumentBuilder. {}", e);
-            throw new JAXBException(e);
-        }
         JAXBContext jc = JAXBContext.newInstance(Rpfs.class);
         Unmarshaller u = jc.createUnmarshaller();
-        rpfs = (Rpfs) u.unmarshal(document);
+        rpfs = (Rpfs) u.unmarshal(inputStream);
     }
 
     /**
-     * Return the data series abbreviation for an RPF file name.
-     * <p>
-     * This encodes MIL-STD-2411-1 Section 5.1.4 data series abbreviations.
-     *
-     * @param fileName the filename to look-up the abbreviation for.
-     * @return standard abbreviation for the data series.
-     */
+        Return the data series abbreviation for an RPF file name.
+        <p>
+        This encodes MIL-STD-2411-1 Section 5.1.4 data series abbreviations.
+
+        @param fileName the filename to look-up the abbreviation for.
+        @return standard abbreviation for the data series.
+    */
     public final String getAbbreviationForFileName(final String fileName) {
         Rpfs.Rpf rpf = getRpfForFileName(fileName);
         if (rpf == null) {
@@ -111,13 +87,13 @@ public class RasterProductFormatUtilities {
     }
 
     /**
-     * Return the data series name for an RPF file name.
-     * <p>
-     * This encodes MIL-STD-2411-1 Section 5.1.4 data series names.
-     *
-     * @param fileName the filename to look-up the name for.
-     * @return standard name for the data series.
-     */
+        Return the data series name for an RPF file name.
+        <p>
+        This encodes MIL-STD-2411-1 Section 5.1.4 data series names.
+
+        @param fileName the filename to look-up the name for.
+        @return standard name for the data series.
+    */
     public final String getNameForFileName(final String fileName) {
         Rpfs.Rpf rpf = getRpfForFileName(fileName);
         if (rpf == null) {
@@ -127,11 +103,11 @@ public class RasterProductFormatUtilities {
     }
 
     /**
-     * Find RPF entry corresponding to a file name.
-     *
-     * @param fileName the filename
-     * @return corresponding RPF.
-     */
+        Find RPF entry corresponding to a file name.
+
+        @param fileName the filename
+        @return corresponding RPF.
+    */
     private Rpfs.Rpf getRpfForFileName(final String fileName) {
         String fileSuffix = null;
         int dotPosition = fileName.lastIndexOf('.');
